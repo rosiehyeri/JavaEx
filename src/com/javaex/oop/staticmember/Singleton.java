7041044544e15f04dadb09fedad99873236221a4
전혜리 <jeonhr0309@gmail.com>
2024-03-26 17:52:30 +0900

    3

---
+++ src/com/javaex/oop/staticmember/Singleton.java
@@ -0,0 +1,16 @@
+package com.javaex.oop.staticmember;
+
+//static 영역 활용
+//단 하나의 인스턴스만 유지
+//new singleton() -> x
+
+public class Singleton {
+	private static Singleton instance = new Singleton();
+	private Singleton() {
+		//new 생성 불가하게됨
+	}
+	public static Singleton getInstance() {
+		// 유일한 인스턴스 반환
+		return instance;
+	}
+}
--- src/com/javaex/oop/tv/TV.java
+++ src/com/javaex/oop/tv/TV.java
@@ -1,7 +1,16 @@
 package com.javaex.oop.tv;
 
+//채널 범위 : 1~255
+//볼륨 범위 : 0~100
 public class TV {
 
+	//실수
+	private static final int MIN_CHANNEL = 1;
+	private static final int MAX_CHANNEL = 255;
+	private static final int MIN_VOLUME = 0;
+	private static final int MAX_VOLUME = 100;
+	
+	//필드
 	private int channel;
 	private int volume;
 	private boolean power;
@@ -9,6 +18,9 @@ public class TV {
 	//생성자 : channel, volume, power의 초기값 각각 7, 20, false
 	public TV() {
 		this(7,20,false);
+		//this.channel = 7;
+		//this.volume = 20;
+		//this.power = false; 라고 해도됨
 	}
 	
 	//생성자 : 초기값 설정
@@ -25,46 +37,48 @@ public class TV {
 	public int getVolume() {
 		return volume;
 	}
-	public boolean getPower() {
+	public boolean isPower() {		//boolean은 getter를 is로 쓰는 것이 관례
 		return power;
 	}
 	
 	//TV 전원 제어
 	public void power(boolean on) {
-		power = on;
+		this.power = on;
 	}
 	
-	//채널 변경
+	//채널 변경 메서드
 	public void channel(int channel) {
-		if (channel>=1 && channel <=255)
-			this.channel = channel;
+		if (channel>=MIN_CHANNEL && channel <= MAX_CHANNEL) {
+			this.channel = channel; }
+		//범위를 벗어나면 채널 변경을 하지 않는다.
 	}
 	
-	//채널 증가 또는 감소
+	//채널 변경 메서드 (오버로딩)
 	public void channel(boolean up) {
 		if (up) {
-            if (channel < 255)
-                channel++;
+            if (this.channel < MAX_CHANNEL) 		// 최대 채널에 도달하지 않음
+                this.channel++;
         } else {
-            if (channel > 1)
-                channel--;
+        	if (this.channel > MIN_CHANNEL)			// 최소 채널에 도달하지 않음
+                this.channel--;
         }
+   
 	}
 	
-	// 볼륨 설정
+	// 볼륨 조절 메서드
     public void volume(int volume) {
-        if (volume >= 0 && volume <= 100)
+        if (volume >= MIN_VOLUME && volume <= MAX_VOLUME)
             this.volume = volume;
     }
 
-    // 볼륨 증가 또는 감소
+    // 볼륨 조절 메서드 (오버로딩)
     public void volume(boolean up) {
         if (up) {
-            if (volume < 100)
-                volume++;
-        } else {
-            if (volume > 0)
-                volume--;
+            if (this.volume < MAX_VOLUME)		// 최대 볼륨에 도달하지 안흠
+                this.volume++;
+        } else {		
+            if (this.volume > MIN_VOLUME)		// 최소 볼륨에 도달하지 않음  
+                this.volume--;
         }
     }
 
